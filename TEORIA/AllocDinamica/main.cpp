57b47f7a2b73de19841698626f2dc5ae101a936a
raffaele <raffaeleventriglia6@gmail.com>
2021-05-02 22:09:02 +0200

    UPDATE

--- TEORIA/AllocDinamica/main.cpp
+++ TEORIA/AllocDinamica/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
 
 int main(){
-    
+    int* pt = new int;  //allocazione dinamica di una variabile
+    float* arr = new float[4];  //allocazione dinamica di un array
+
+    if(pt == nullptr) exit(1);  //controllo riuscita allocazione
+    if(arr == nullptr) exit(1);  //controllo riuscita allocazione
+
+    *pt = 5;
+    cout << *pt << endl;
+
+    srand((unsigned) time(NULL));
+    for(int i = 0; i < 4; i++){
+        arr[i] = rand() % 100;
+    }
+
+    for(int i = 0; i < 4; i++){
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+
+    delete pt;  //deallocazione dinamica
+    delete [] arr;  //deallocazione dinamica
+
+    cout << *pt << endl;
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
     return 0;
 }
