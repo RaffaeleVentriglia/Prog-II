7e867babb48a9ad8692db3f5a14b0fbbbd69859b
raffaele <raffaeleventriglia6@gmail.com>
2021-05-04 23:10:36 +0200

    update

--- ESERCITAZIONI/Automobile/main.cpp
+++ ESERCITAZIONI/Automobile/main.cpp
@@ -1,22 +1,87 @@
-#include <iostream>
-#include <string>
-using namespace std;
 #include "main.hpp"
 
-int main(){
-    unsigned int scelta;
-    unsigned int vel;
-    Automobile aut("Suv");
-    cout << "Tipo: " << aut.getType() << endl;
-    cout << "Stato: " << aut.getState() << endl;
-    cout << "Velocità: " << aut.getSpeed() << endl;
-
-    while(scelta == 3){
-        switch(scelta){
-            case 1:
-                cout << "Di quanto vuoi accelerare: ";
-                cin >> vel;
-                aut.Accelera(vel);
+int main() {
+    Auto a1("Panda");
+    int scelta;
+    float vel;
+    cout << "Tipo di macchina: " << a1.getType() << endl;
+    while (scelta != 0){
+        if(!a1.getState()){
+            cout << "0. Esci dal programma" << endl;
+            cout << "1. Accendi l'auto" << endl;
+            cout << "2. Stato dell'auto" << endl;
+            cout << "Scegliere l'azione da compiere: ";
+            cin >> scelta;
+            switch (scelta) {
+                case 0:
+                    break;
+                case 1:
+                    a1.setState(true);
+                    break;
+                case 2:
+                    cout << "Stato: ";
+                    if (!a1.getState()){
+                        cout << "Accesa" << endl;
+                    } else {
+                        cout << "Spenta" << endl;
+                    }
+                    break;
+                default:
+                    cout << "Errore nell'inserimento dell'azione" << endl;
+                    exit(1);
+            }
+        } else {
+            cout << "1. Spegni l'auto" << endl;
+            if(a1.getSpeed() == 0){
+                cout << "2. Accelera" << endl;
+                cout << "Scegliere l'azione da compiere: ";
+                cin >> scelta;
+                switch (scelta) {
+                    case 0:
+                        break;
+                    case 1:
+                        a1.setState(false);
+                        a1.azzera();
+                        break;
+                    case 2:
+                        cout << "Di quanto si vuole accelerare: ";
+                        cin >> vel;
+                        a1.accelera(vel);
+                        cout << "Velocità: " << a1.getSpeed() << endl;
+                        break;
+                    default:
+                        cout << "Errore nell'inserimento dell'azione" << endl;
+                        exit(1);
+                }
+            } else {
+                cout << "2. Accelera ancora" << endl;
+                cout << "3. Decelera" << endl;
+                cout << "Scegliere l'azione da compiere: ";
+                cin >> scelta;
+                switch (scelta) {
+                    case 0:
+                        break;
+                    case 1:
+                        a1.setState(false);
+                        a1.azzera();
+                        break;
+                    case 2:
+                        cout << "Di quanto si vuole accelerare: ";
+                        cin >> vel;
+                        a1.accelera(vel);
+                        cout << "Velocità: " << a1.getSpeed() << endl;
+                        break;
+                    case 3:
+                        cout << "Di quanto si vuole decelerare: ";
+                        cin >> vel;
+                        a1.decelera(vel);
+                        cout << "Velocità: " << a1.getSpeed() << endl;
+                        break;
+                    default:
+                        cout << "Errore nell'inserimento dell'azione" << endl;
+                        exit(1);
+                }
+            }
         }
     }
     return 0;
--- ESERCITAZIONI/Automobile/main.hpp
+++ ESERCITAZIONI/Automobile/main.hpp
@@ -1,20 +1,51 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Automobile {
-    protected:
-        bool state = false;
-        float speed = 0.0;
-        string type;
-    public:
-        Automobile(string type) {
-            this -> type = type;
-        };
-        // void setType(string t) {this -> type = t;};
-        string getType() {return this -> type;};
-        void setState(bool s) { this -> state = s;};
-        bool getState() {return this -> state;};
-        void Accelera(float v) { this -> speed += v;};
-        void Decelera(float v) { this -> speed -= v;};
-        float getSpeed() {return this -> speed;};
+
+class Auto {
+protected:
+    bool state = false;
+    float speed = 0.0;
+    string type;
+public:
+    explicit Auto(string type);
+    string getType();
+    void setState(bool s);
+    bool getState();
+    void accelera(float v);
+    void decelera(float v);
+    void azzera();
+    float getSpeed();
 };
+
+Auto::Auto(string type) {
+    this -> type = std::move(type);
+}
+
+string Auto::getType() {
+    return this -> type;
+}
+
+void Auto::setState(bool s) {
+    this -> state = s;
+}
+
+bool Auto::getState() {
+    return this -> state;
+}
+
+void Auto::accelera(float v) {
+    this -> speed += v;
+}
+
+void Auto::decelera(float v) {
+    this -> speed -= v;
+}
+
+void Auto::azzera(){
+    this -> speed = 0;
+}
+
+float Auto::getSpeed() {
+    return this -> speed;
+}
