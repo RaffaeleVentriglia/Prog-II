a4d57e60605c60f489ba9a2189a781bfebdb9bc8
raffaele <raffaeleventriglia6@gmail.com>
2021-05-28 12:35:32 +0200

    new exercise

--- TEORIA/Tree/BinaryTree.c
+++ TEORIA/Tree/BinaryTree.c
@@ -1,100 +1,34 @@
+// FILE DEDICATO ALLA SOLA COSTRUZIONE E VISITA CON I TRE METODI DI UN ALBERO BINARIO
+
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef char DATA;
+typedef struct nodo {  // struttura per la rappresentazione di un nodo
+    int data;     // informazione contenuta nel nodo corrente
+    struct nodo *sx;     // puntatore al figlio sinistro
+    struct nodo *dx;     // puntatore al figlio destro
+} nodo;
 
-struct node {
-    DATA d;
-    struct node *left;
-    struct node *right;
-};
+typedef nodo* tree;  // in questo modo avremo la variabile tree puntatore a variabile di tipo nodo
 
-typedef struct node NODE;
-typedef NODE *BTREE;
-
-// COSTRUZIONE DI UN ALBERO BINARIO
-BTREE new_node(void){
-    return (malloc(sizeof(NODE)));
+tree costruisci_albero(int data, tree sx, tree dx){
+    tree radice = (nodo *)malloc(sizeof(nodo));  // allochiamo spazio in memoria per la costruzione della radice
+    radice -> data = data;  // assegniamo data passato come parametro al data puntato da radice
+    radice -> sx = sx;      // assegniamo sx passato come parametro a sx puntato da radice
+    radice -> dx = dx;      // assegniamo dx passato come parametro a dx puntato da radice
+    return (radice);
 }
 
-BTREE init_node(DATA d1, BTREE p1, BTREE p2){
-    BTREE t;
-    t = new_node();
-    t -> d = d1;
-    t -> left = p1;
-    t -> right = p2;
-    return t;
-}
-
-// CREAZIONE DI UN ALBERO BINARIO DA UN ARRAY
-BTREE create_tree(DATA a[], int i, int size){
-    if(i >= size){
-        return NULL;
-    } else {
-        return (init_node(a[i], create_tree(a, 2*i+1, size), create_tree(a, 2*i+2, size)));
-    }
-}
-
-// visita di un albero binario in ordine simmetrico
-void inorder(BTREE root){
-    if(root != NULL){
-        inorder(root -> left);
-        printf("%c -> ", root -> d);
-        inorder(root -> right);
-    }
-}
-
-// visita di un albero binario in ordine posticipato
-void postorder(BTREE root){
-    if(root != NULL){
-        postorder(root -> left);
-        postorder(root -> right);
-        printf("%c -> ", root -> d);
-    }
-}
-
-//visita di un albero binario in ordine anticipato
-void preorder(BTREE root){
-    if(root != NULL){
-        printf("%c -> ", root -> d);
-        preorder(root -> left);
-        preorder(root -> right);
-    }
+tree albero_vuoto(){
+    return (NULL);
 }
 
 int main(){
-    char a[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'L'};
-    inorder(create_tree(a, 3, 10));
-    printf("\n");
-    postorder(create_tree(a, 3, 10));
-    printf("\n");
-    preorder(create_tree(a, 3, 10));
-    printf("\n\n");
 
-
-    inorder(create_tree(a, 8, 10));
-    printf("\n");
-    postorder(create_tree(a, 8, 10));
-    printf("\n");
-    preorder(create_tree(a, 8, 10));
-    printf("\n\n");
-
-
-    inorder(create_tree(a, 2, 10));
-    printf("\n");
-    postorder(create_tree(a, 2, 10));
-    printf("\n");
-    preorder(create_tree(a, 2, 10));
-    printf("\n\n");
-
-
-    inorder(create_tree(a, 0, 10));
-    printf("\n");
-    postorder(create_tree(a, 0, 10));
-    printf("\n");
-    preorder(create_tree(a, 0, 10));
-    printf("\n\n");
-
-
+    // costruzione di un albero con radice 5, sx: 7 e dx: 3
+    tree t1, t2, t3;
+    t2 = costruisci_albero(7, albero_vuoto(), albero_vuoto());
+    t3 = costruisci_albero(3, albero_vuoto(), albero_vuoto());
+    t1 = costruisci_albero(5, t2, t3);
     return 0;
 }
